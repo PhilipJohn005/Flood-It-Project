3b5b2779a514d3bcabf3256fec80c983a9ea46ce
PhilipJohn005 <pjdeluge123@gmail.com>
2025-06-30 17:24:30 +0530

    Modified for clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,18 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
-import { Button } from "@/components/ui/button";
-import SigninButton from "@/components/pages/landing-page/SigninButton";
-import Image from "next/image";
+import Header from "@/components/pages/landing-page/Header";
+import Afterheader from "@/components/pages/landing-page/Afterheader";
+
 
 export default function LoginForm() {
-  const { data: session, status } = useSession();
-  const loading = status === "loading";
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
 
   return (
     <div>
-      <header className="border-b shadow-lg p-4">
-        <div className="max-w-6xl mx-auto flex items-center justify-between">
-          <div className="flex items-center space-x-4">
-            <h1 className="font-bold text-2xl">FloodFill Arena</h1>
-          </div>
-          <div>
-            {session?.user ? (
-              <div className="flex items-center gap-4">
-                {session.user.image && (
-                  <Image
-                    src={session.user.image}
-                    alt="profile"
-                    width={40}
-                    height={40}
-                    className="w-10 h-10 rounded-full"
-                  />
-                )}
-                <div className="text-white dark:text-black">
-                  <p className="text-sm font-semibold">{session.user.name}</p>
-                  <p className="text-xs">{session.user.email}</p>
-                </div>
-                <Button
-                  onClick={() => signOut({ callbackUrl: "/" })}
-                  className="rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700 transition"
-                >
-                  Logout
-                </Button>
-              </div>
-            ) : (
-              <SigninButton />
-            )}
-          </div>
-            
-        </div>
-      </header>
+      <Header/>
+
+      <div className="p-4">
+        <Afterheader/>
+      </div>
       
     </div>
   );
